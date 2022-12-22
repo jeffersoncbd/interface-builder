b7d0172a24870b906702aaace2bcaa29e84f0f55
Jefferson Carlos <jeff.carlosbd@gmail.com>
2022-12-22 16:42:37 -0300

    moves the function to the tool module

--- src/builder/tools.rs
+++ src/builder/tools.rs
@@ -1,6 +1,10 @@
-use std::io::stdout;
+use std::{io::stdout, time::Duration};
 
-use crossterm::{execute, cursor, terminal::{self, ClearType}};
+use crossterm::{
+  execute, cursor,
+  terminal::{self, ClearType, enable_raw_mode, disable_raw_mode},
+  event::{KeyCode, Event, self, poll}
+};
 
 pub fn clear_terminal() {
   execute!(
@@ -16,4 +20,34 @@ pub fn move_cursor_to(x: u16, y: u16) {
     stdout(),
     cursor::MoveTo(x, y)
   ).expect("Failed to move cursor");
+}
+
+pub fn await_key_code(key_code: KeyCode) {
+  enable_raw_mode().expect("Failed on enable raw mode");
+  execute!(
+    stdout(),
+    cursor::MoveTo(0,0),
+    cursor::Hide
+  ).expect("Move failed");
+  loop {
+    if poll(Duration::from_millis(50)).expect("Error") {
+      let event = event::read().expect("Failed on read event");
+
+      if event == Event::Key(key_code.into()) {
+        execute!(
+          stdout(),
+          cursor::Show
+        ).expect("Cleaning failed");
+        break;
+      }
+    } else {
+      execute!(
+        stdout(),
+        cursor::MoveTo(0,0),
+        terminal::Clear(ClearType::CurrentLine),
+        cursor::Hide
+      ).expect("Cleaning failed");
+    }
+  }
+  disable_raw_mode().expect("Failed on disable raw mode");
 }
